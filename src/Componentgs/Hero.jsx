ac472ecefbd405127b767248739f6e287b72d0af
Sarah Angyu <angyusarah1@gmail.com>
2024-04-09 14:40:41 +0100

    created pages

---
+++ src/Componentgs/Hero.jsx
@@ -0,0 +1,13 @@
+const Hero = () => {
+    return (
+      <div>
+          <div>
+              <h1>Lorem ipsum dolor sit amet consectetur.</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel accusamus laboriosam quisquam debitis!</p>
+              <button>Contact Us</button>
+          </div>
+      </div>
+    )
+  }
+  
+  export default Hero
---
+++ src/Pages/LandingPage.jsx
@@ -0,0 +1,11 @@
+import Hero from "../Componentgs/Hero"
+
+const LandingPage = () => {
+  return (
+    <div>
+        <Hero/>
+    </div>
+  )
+}
+
+export default LandingPage
