082f1658c1b3e5cdbfff30494d884c86331d8a2e
Sarah Angyu <angyusarah1@gmail.com>
2024-04-11 15:38:46 +0100

    fixed bug in the laning page

--- src/Components/Container.jsx
+++ src/Components/Container.jsx
@@ -3,7 +3,7 @@ import Header from "./Header"
 
 const Container = ({children}) => {
   return (
-    <div className="flex flex-col gap-4 lg:px-24">
+    <div className="flex flex-col gap-4 ">
       <Header/>
       {children}
       <Footer/>
