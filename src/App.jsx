af7ca1fc582e36fa4c860d8622618f5ae21026de
Yngauxide <danieloxide4all@gmail.com>
2024-04-15 08:42:22 -0700

    services

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-<<<<<<< HEAD
+
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import LandingPage from "./Pages/LandingPage"
 import ContactPage from "./Pages/ContactPage"
-import Service from "./Components/Service"
 import ServicesPage from "./Pages/ServicesPage"
-=======
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import LandingPage from "./Pages/LandingPage";
-import ContactPage from "./Pages/ContactPage";
-import AboutPage from "./Pages/AboutPage";
->>>>>>> 144c7cbd5bf5da7c9d25d2476606c2e94e0541b2
+import AboutPage from "./Pages/AboutPage"
+
 
 function App() {
   return (
