eda34a51d937472fb9c9fca5fb162dbc4f239692
Janet Bello <bellojanet022@gmail.com>
2024-04-16 12:41:26 +0100

    added a Navdata file

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,4 +1,5 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import { navlinks } from "./Navdata";
 
 export default function Nav({ hideNav }) {
   return (
@@ -6,7 +7,7 @@ export default function Nav({ hideNav }) {
       className={`${hideNav ? "hidden" : ""} absolute md:static md:block md:mx-0 top-0 right-0 left-0 mx-auto flex justify-center   h-[30vh] md:h-full bg-white`}
     >
       <ul className={"flex flex-col md:flex-row gap-14 pt-8 md:pt-0"}>
-        <li>
+        {/* <li className={({isActive}) => isActive ? 'text-red-500' : ''}>
           <Link to={"/"}>Home</Link>
         </li>
         <li>
@@ -17,7 +18,16 @@ export default function Nav({ hideNav }) {
         </li>
         <li>
           <Link to={"/contact"}>Contact</Link>
-        </li>
+        </li> */}
+        {
+          navlinks.map(({name, path}, index) => {
+            return (
+              <li className=" font-semibold" key={index}>
+                <NavLink to={path} className={({isActive}) => isActive ? 'text-footerblue font-extrabold' : ''}>{name}</NavLink>
+              </li>
+            )
+          })
+        }
       </ul>
     </nav>
   );
---
+++ src/Components/Navdata.js
@@ -0,0 +1,6 @@
+export const navlinks = [
+    {name: 'Home', path: '/'},
+    {name: 'About', path: '/about'},
+    {name: 'Services', path: '/services'},
+    {name: 'Contact', path: '/contact'},
+]
