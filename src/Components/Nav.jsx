e69a8fa4fcce643921c2e1d5bec6b0fdc63b729d
Elrey009 <daniloejike@gmail.com>
2024-04-16 12:07:06 +0100

    modified nav links

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -3,19 +3,21 @@ import { Link } from "react-router-dom";
 export default function Nav({ hideNav }) {
   return (
     <nav
-      className={`${hideNav ? "hidden" : ""} absolute md:static md:block md:mx-0 top-0 right-0 left-0 mx-auto flex justify-center   h-[30vh] md:h-full bg-white`}
+      className={`${
+        hideNav ? "hidden" : ""
+      } absolute md:static md:block md:mx-0 top-0 right-0 left-0 mx-auto flex justify-center   h-[30vh] md:h-full bg-white`}
     >
       <ul className={"flex flex-col md:flex-row gap-14 pt-8 md:pt-0"}>
-        <li>
+        <li className="  hover:text-footerblue">
           <Link to={"/"}>Home</Link>
         </li>
-        <li>
+        <li className="  hover:text-footerblue">
           <Link to={"/about"}>About</Link>
         </li>
-        <li>
+        <li className="  hover:text-footerblue">
           <Link to={"/services"}>Services</Link>
         </li>
-        <li>
+        <li className="  hover:text-footerblue ">
           <Link to={"/contact"}>Contact</Link>
         </li>
       </ul>
